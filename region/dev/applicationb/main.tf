9684f41eef411a363d385e970994a7d1af4fb2c7
Raj Kondapuli (Contractor) <Raj.Kondapuli@wnco.com>
2021-06-03 15:02:25 -0500

    New Some Test Commit New

--- region/dev/applicationb/main.tf
+++ region/dev/applicationb/main.tf
@@ -11,13 +11,13 @@ data "test label aa" "test secret aa" {
   name = "path/test"
 }
 provider "testarkaabb" {
-  base_url        = var.base_url
-  username        = var.username
-  password        = var.password
   auth_path       = var.login_path
   log_level       = "DEBUG"
   debug_requests  = true
   debug_responses = true
+  base_url        = var.base_url
+  username        = var.username
+  password        = var.password
 }
 data "test label aabb" "test secret aabb" {
   name = "path/test"
